f5b051038543699f285aee96753fa38e5d7cff55
leviathandavid <shalashaska-1992@hotmail.com>
2017-07-10 23:03:59 -0500

    hola mundo en joption pane

--- Aplicacion_prueba_git/src/Main.java
+++ Aplicacion_prueba_git/src/Main.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,7 +14,7 @@
 public class Main {
     
     public static void main(String[]a){
-        System.out.println("hola mundo");
+        JOptionPane.showInternalMessageDialog(null, "Hola mundo");
     
     }
     
