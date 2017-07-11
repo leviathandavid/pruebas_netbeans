695693a5da3805ef6a7f72f3ab4dd95510b91b07
leviathandavid <shalashaska-1992@hotmail.com>
2017-07-10 22:55:42 -0500

    primeros archivos cargados desde netbeans

---
+++ Aplicacion_prueba_git/src/aplicacion_prueba_git/Aplicacion_prueba_git.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacion_prueba_git;
+
+/**
+ *
+ * @author David
+ */
+public class Aplicacion_prueba_git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
